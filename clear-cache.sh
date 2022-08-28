6fc0766d4e09dab57b2a21dd883237d7093008bd
Ryan Qian <i@bitbili.net>
2022-08-28 23:07:03 +0800

    clear-cache.sh: can be used to clear cache, e.g.: Clean caches that are incompatible with previous commits

---
+++ clear-cache.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# @author cwittlut <i@bitbili.net>
+#
+
+set -e
+
+_my_path=$(dirname $(realpath $0))
+. "${_my_path}/env"
+
+# help
+if [[ $1 =~ ^- ]]; then
+  echo "
+Usage: $0 <ID>... | <PATH>...
+"
+  exit
+fi
+
+declare -a _rm_work_dirs
+for _a; do
+  if [[ ${_a} =~ ^[_0-9a-zA-Z]{5}(\.id)?$ ]]; then
+    _work_dir="$(find ${_home_dir} -maxdepth 2 -name ${_a%.id}'.id' -type f -printf '%h\n')"
+  else
+    _work_dir="${_home_dir}/${_a##*/}"
+  fi
+  _cache_dir=$(ls -1d ${_work_dir}/tmp.*)
+  _rm_cache_dirs+=("rm -rf ${_cache_dir}"$'\n')
+done
+
+if [[ ${#_rm_cache_dirs[@]} -lt 1 ]]; then
+  exit
+fi
+
+set -- "${_rm_cache_dirs[@]}"
+echo " ""${@}"
+eval "${@}"
