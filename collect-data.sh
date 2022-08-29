eadeaebb45e58f0fcae2ce37c0d81ebb808b2e5f
Ryan Qian <i@bitbili.net>
2022-08-29 22:31:39 +0800

    collect-data.sh: make it can append/stop jobs in real time

--- collect-data.sh
+++ collect-data.sh
@@ -9,9 +9,10 @@ _my_path=$(dirname $(realpath $0))
 . "${_my_path}/env"
 
 # help
-if [[ $1 =~ ^- ]]; then
+if [[ $1 =~ ^- && $1 != '-s' ]]; then
   echo "
-Usage: $0 <ip>...
+Usage: ${0##*/} <IP>...       start collect data job(s) or append new job(s) to the current process
+       ${0##*/} -s <ID>...    stop job(s) of the specified ID(s)
 "
   exit
 fi
@@ -42,27 +43,33 @@ _check_ipv6() {
   return ${_ret}
 }
 
+_insert_time_exec() {
+  set -- echo "===== $(_date)"
+  echo "${@}" ">>""${1}" >${_debug_log}
+  "${@}" >>"${1}"
+}
 #
 # $1: file
 _insert_time() {
-  local _running_file=${1%/*}/RUNNING
+  local _ping_file=${1}
   trap '
-    echo "===== $(_date)" >>"${1}"
-    rm ${_running_file}
+    exit 130
   ' SIGINT
-  touch ${_running_file}
+  trap '
+    _insert_time_exec "${_ping_file}"
+  ' ERR EXIT
   while :; do
-    echo "===== $(_date)" >>"${1}"
+    _insert_time_exec "${_ping_file}"
     sleep 600
   done
 }
 
-# main process
-declare -a _ping_subprocesses _insert_subprocesses
-for _ip; do
-  _c_date=$(_date '+D%Y-%m-%d_T%H-%M-%S_%z')
-  _opts=''
-
+#
+# $1: ip
+_append_task() {
+  local _c_date=$(_date '+D%Y-%m-%d_T%H-%M-%S_%z')
+  local _opts=''
+  local _ip="$1"
   if _check_ipv4 ${_ip}; then
     _work_dir="ping_v4_${_ip//./-}_${_c_date}"
     _opts="-4"
@@ -70,25 +77,27 @@ for _ip; do
     _work_dir="ping_v6_${_ip//:/-}_${_c_date}"
     _opts="-6"
   else
-    echo "Invaild IP address!" >&2
-    exit 1
+    set -- A invalid ${_ip}
+    _show_info "${@}"
+    echo "${@}" >${_NC_FIFO}
+    return 1
   fi
 
   _work_dir="${_home_dir}/${_work_dir}"
   set -- mkdir -p "${_work_dir}"
-  echo ">>>" "${@}"
+  echo ">>>" "${@}" >${_debug_log}
   "${@}"
 
   _ping_file="${_work_dir}/ping.log"
 
-  echo ">>>" "do inserting time job ..."
+  echo ">>>" "do inserting time job ..." >${_debug_log}
   ( _insert_time "${_ping_file}" ) &
-  _insert_subprocesses+=($!)
+  _insert_subprocesses=$!
 
   set -- ping ${_opts} ${_ip}
-  echo ">>>" "${@}" " ..."
+  echo ">>>" "${@}" " ..." >${_debug_log}
   ( "${@}" &>>"${_ping_file}" ) &
-  _ping_subprocesses+=($!)
+  _ping_subprocesses=$!
 
   # generate a unique id
   _existing_ids=($(find ${_home_dir} -maxdepth 2 -name '*.id' -type f -printf '%f\n'))
@@ -111,25 +120,133 @@ for _ip; do
     fi
   done
   set -- touch "${_work_dir}/${_this_id}.id"
-  echo ">>>" "${@}"
+  echo ">>>" "${@}" >${_debug_log}
   "${@}"
 
-  echo "
-      IP: ${_ip}
-      ID: ${_this_id}
-work dir: ${_work_dir}
-ping log: ${_ping_file}
-"
-done
+  eval "_id_pid_map[${_this_id}]='${_insert_subprocesses} ${_ping_subprocesses} ${_ip} ${_work_dir}'"
 
-echo "<CTRL-C> to finish it"
+  set -- A ${_ip} ${_this_id} ${_work_dir}
+  _show_info "${@}"
+  echo "${@}" >${_NC_FIFO}
+}
+
+#
+# $1: id
+_stop_task() {
+  local _insert_subprocesses _ping_subprocesses _ip _work_dir
+  read -r _insert_subprocesses _ping_subprocesses _ip _work_dir <<<"${_id_pid_map[${1}]}"
+  if [[ -z ${_insert_subprocesses} && -z ${_ping_subprocesses} ]]; then
+    set -- S invalid ${1}
+    _show_info "${@}"
+    echo "${@}" >${_NC_FIFO}
+    return
+  fi
+  kill -9 ${_insert_subprocesses}
+  kill -9 ${_ping_subprocesses}
+  _insert_time_exec "${_work_dir}/ping.log"
+  set -- S ${_ip} ${1} ${_work_dir}
+  _show_info "${@}"
+  echo "${@}" >${_NC_FIFO}
+}
+
+_listen() {
+  set +e
+  local _ip
+  local -A _id_pid_map
+  while :; do
+    read -r _item _action
+    case ${_action} in
+      append)
+        _append_task "${_item}"
+        ;;
+      stop)
+        _stop_task "${_item}"
+        ;;
+      socket_test)
+        echo "ok" >${_NC_FIFO}
+        ;;
+    esac
+  done
+}
+
+#
+# $1: A|R
+_show_info() {
+  local _action=${1}
+  local _ip=${2}
+  local _id=${3}
+  local _work_dir=${4}
+  case ${_action} in
+    A)
+      if [[ ${_ip} != "invalid" ]]; then
+        echo -en "\033[32m
+New item appended:\033[0m"
+      else
+        echo "Invalid IP address '${_id}'!" >&2
+      fi
+      ;;
+    S)
+      if [[ ${_ip} != "invalid" ]]; then
+        echo -en "\033[33m
+Item stopped:\033[0m"
+      else
+        echo "Invalid item ID '${_id}'!" >&2
+      fi
+      ;;
+  esac
+  if [[ ${_ip} != "invalid" ]]; then
+    echo "
+        IP: ${_ip}
+        ID: ${_id}
+  Work Dir: ${_work_dir}
+"
+  fi
+}
+
+_send_item() {
+  echo "$*" | nc -W 1 -U ${_sock}
+}
 
 trap '
-for _subid in ${_ping_subprocesses[@]} ${_insert_subprocesses[@]}; do
-  kill -s SIGINT ${_subid} && \
-    echo "subprocess ${_subid} killed." || \
-    echo "kill subprocess ${_subid} failed."
-done
-' EXIT
+if [[ -n ${_socket_server} ]]; then
+  kill -9 ${_socket_server} && \
+    { \
+      echo "socket server killed."; \
+      rm ${_sock}; \
+    } || \
+    echo "kill socket server failed."
+fi
+if [[ -e ${_NC_FIFO} ]]; then
+  rm -f ${_NC_FIFO}
+fi
+' ERR EXIT
+
+# main process
+_sock="/tmp/network-connectivity-test.sock"
+if [[ -e ${_sock} ]]; then
+  _action='append'
+  if [[ ${1} == '-s' ]]; then
+    _action='stop'
+    shift
+  fi
+  for _item; do
+    _show_info $(_send_item ${_item} ${_action})
+  done
+else
+  _NC_FIFO="/tmp/network-connectivity-test.fifo"
+  mkfifo ${_NC_FIFO}
+  exec {_FD_ITEM}> >(_listen)
+  _listen_pid=$!
+  ( < <(tail -f ${_NC_FIFO}) nc -k -l -U ${_sock} >&${_FD_ITEM} ) &
+  _socket_server=$!
+  while :; do
+    if [[ $(echo '_ socket_test' | nc -W 1 -U ${_sock} 2>${_debug_log}) == 'ok' ]]; then
+      break
+    fi
+  done
+  for _ip; do
+    _send_item ${_ip} append >/dev/null
+  done
+fi
 
 wait
